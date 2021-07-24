81e62f72b591eca1a1987424fba6fc5006f81e2d
Ethan Howes <eth11659@esri.com>
2021-07-23 20:58:59 -0400

    initial ethan commit

--- app/src/main/java/com/gismo/safejourney/MainActivity.kt
+++ app/src/main/java/com/gismo/safejourney/MainActivity.kt
@@ -2,7 +2,7 @@ package com.gismo.safejourney
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+// created by gismo
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
